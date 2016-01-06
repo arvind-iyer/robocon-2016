e5d291deb982e4705bb6947f43d346368117b7e3
Christopher Wong <pkcwong@users.noreply.github.com>
2016-01-06 21:16:12 +0800

    Update motor.cpp

--- algorithm/motor.cpp
+++ algorithm/motor.cpp
@@ -10,10 +10,10 @@ void _move(float X, float Y, float W)
 	int M1;
 	int M2;
 	int M3;
-	M1 = X*2+W;
-	M2 = (Y-X*sqrt(3))/sqrt(3);
-	M3 = -W-M1-M2;
-	//printf("_move\t\tM1: %d\tM2: %d\tM3: %d\n", M1, M2, M3);
+	M1=(-W-X*2)/3;
+	M2=(-W*sqrt(3)/3+X*sqrt(3)/3-Y)/sqrt(3);
+	M3=-W-M1-M2;
+	printf("\n_move\nM1: %d\nM2: %d\nM3: %d\n\n", M1, M2, M3);
 }
 
 void _move(int magnitude, int bearing)
@@ -25,9 +25,18 @@ int main()
 {
 	while (1)
 	{
+		
+		int x;
+		int y;
+		int w;
+		scanf("%d %d %d", &x, &y, &w);
+		_move(x, y, w);
+		
+		/*
 		int m;
 		int b;
 		scanf("%d %d", &m, &b);
 		_move(m, b);
+		*/
 	}
 }
