bf28dc1ba5ffd72c83578eabebc4498ec8b911cf
Hong Wing Pang <panghw130@gmail.com>
2016-01-07 07:42:31 +0800

    Added correction vector

--- hkust-robocon-mb/stm32f103VF_template/src/user/main.c
+++ hkust-robocon-mb/stm32f103VF_template/src/user/main.c
@@ -5,6 +5,7 @@
 #define THRESHOLD 50
 
 int tar_x, tar_y, cur_x, cur_y, cur_deg;
+int cor_x, cor_y;
 int v1, v2, v3;
 int degree, dist, speed;
 bool stopped;
@@ -24,12 +25,13 @@ void can_motor_stop(){
 	v1 = 0;
 	v2 = 0;
 	v3 = 0;
+	motor_set_vel(MOTOR1, v1, CLOSE_LOOP);
+	motor_set_vel(MOTOR2, v2, CLOSE_LOOP);
+	motor_set_vel(MOTOR3, v3, CLOSE_LOOP);
 	motor_lock(MOTOR1);
 	motor_lock(MOTOR2);
 	motor_lock(MOTOR3);
 }
-
-//deceleration
 
 int main(void)
 {
@@ -50,21 +52,37 @@ int main(void)
 	tar_x = 0;
 	tar_y = 0;
 	_delay_ms(2000);
+	//set initial target pos
 	tar_x = 0;
-	tar_y = 2000;
+	tar_y = 1500;
 	stopped = false;
 	
 	while (1) {
 		if (get_ticks() % 50 == 0) {
-			cur_x = get_X();
-			cur_y = get_Y();
 			cur_deg = get_angle();
+			cor_x = 0;
+			cor_y = 375;
+			xy_rotate(&cor_x, &cor_y, (-1)*cur_deg);
+			cur_x = get_X() + cor_x;
+			cur_y = get_Y() + cor_y;
 			degree = 90 - int_arc_tan2(tar_y - cur_y, tar_x - cur_x) - (int)(cur_deg/10);
 			dist = Sqrt(Sqr(tar_x - cur_x) + Sqr(tar_y - cur_y));
+			
+			/*
+			//Self-rotation to measure correction vector
+			if (cur_deg <= 1800) {
+				motor_set_vel(MOTOR1, -20, CLOSE_LOOP);
+				motor_set_vel(MOTOR2, -20, CLOSE_LOOP);
+				motor_set_vel(MOTOR3, -20, CLOSE_LOOP);
+			} else {
+				can_motor_stop();
+			}
+			*/
 			
 			if ((Abs(tar_x - cur_x) < THRESHOLD) && (Abs(tar_y - cur_y) < THRESHOLD)) {
 				can_motor_stop();
 			} else {
+				//decelerate
 				if (dist < 300) {
 					speed = (int)(dist/6);
 				} else {
@@ -72,7 +90,7 @@ int main(void)
 				}
 				can_motor_set_angle(degree, speed);
 			}
-			
+						
 			tft_clear();
 			tft_prints(0,0,"CUR X: %d",cur_x);
 			tft_prints(0,1,"CUR Y: %d",cur_y);
