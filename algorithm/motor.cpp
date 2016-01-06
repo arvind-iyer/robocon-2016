2dd4a2e5c20b4e22157b57e1336786e950d9906e
Christopher Wong <pkcwong@users.noreply.github.com>
2016-01-06 20:49:56 +0800

    Update motor.cpp

--- algorithm/motor.cpp
+++ algorithm/motor.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <math.h>
+
+#define PI 3.1415926535897932
+
 using namespace std;
 
 void _move(float X, float Y, float W)
@@ -10,17 +13,21 @@ void _move(float X, float Y, float W)
 	M1 = X*2+W;
 	M2 = (Y-X*sqrt(3))/sqrt(3);
 	M3 = -W-M1-M2;
-	printf("_move\t\tM1: %d\tM2: %d\tM3: %d\n", M1, M2, M3);
+	//printf("_move\t\tM1: %d\tM2: %d\tM3: %d\n", M1, M2, M3);
+}
+
+void _move(int magnitude, int bearing)
+{
+	_move(magnitude*sin(bearing*PI/360), magnitude*cos(bearing*PI/360), 0);
 }
 
 int main()
 {
 	while (1)
 	{
-		int x;
-		int y;
-		int w;
-		scanf("%d %d %d", &x, &y, &w);
-		_move(x, y, w);
+		int m;
+		int b;
+		scanf("%d %d", &m, &b);
+		_move(m, b);
 	}
 }
