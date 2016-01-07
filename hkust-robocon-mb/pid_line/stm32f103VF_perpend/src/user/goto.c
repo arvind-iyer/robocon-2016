4dfeaf0e0e20081b0fd5cf7850b3416efe3ed329
Rex <holyrexcheng@gmail.com>
2016-01-07 21:52:58 +0800

    corrected perpendiclar formula

--- hkust-robocon-mb/pid_line/stm32f103VF_perpend/src/user/goto.c
+++ hkust-robocon-mb/pid_line/stm32f103VF_perpend/src/user/goto.c
@@ -6,6 +6,7 @@ int16_t V1, V2, V3;
 int16_t track_m, track_c;
 int16_t start_x, start_y;
 int16_t para_bearing;
+int16_t k_value;
 
 uint8_t isRunning(){
 	return running == 1;
@@ -44,6 +45,7 @@ int16_t pid(int16_t errorDis){
 //	}
 }
 
+//Setup the target and values to help calculation
 void setTargetPos(int16_t x, int16_t y){
 	start_x = get_X();
 	start_y = get_Y();
@@ -52,8 +54,10 @@ void setTargetPos(int16_t x, int16_t y){
 	target_x = x;
 	target_y = y;
 
-	track_m = (target_y - start_y)*10/(target_x - start_x);
-	track_c = start_x*10 - track_m * start_y;
+	//Scaled by 100
+	track_m = (target_y - start_y)*100/(target_x - start_x);
+	track_c = start_x*100 - track_m * start_y;
+	k_value = (int16_t) sqrt(track_m*track_m+1*100*100);
 
 	para_bearing = 900 - int_arc_tan2(target_y - start_y, target_x - start_x)*10 + curr_angle;
 
@@ -73,10 +77,12 @@ int16_t distance(int16_t x1, int16_t y1, int16_t x2, int16_t y2){
 	return ((int16_t) sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)));
 }
 
-void get_ppcor(s16 current_x,s16 current_y,s16* ppd_x,s16* ppd_y)
-{
-	*ppd_x = ((current_y - track_c)* track_m + current_x)/(1+track_m*track_m);
-	*ppd_y = (track_m * *ppd_x + track_c)/10;
+//Ref: http://www.intmath.com/plane-analytic-geometry/perpendicular-distance-point-line.php
+//Get the perpendicular distance from the current point to the target line
+int16_t get_ppdis(s16 curr_x,s16 curr_y){
+	int32_t upperPart = track_m*curr_x - curr_y*100 + track_c;
+	upperPart = upperPart < 0 ? -upperPart : upperPart;
+	return upperPart/k_value;
 }
 
 
@@ -90,18 +96,17 @@ void easyStop() {
 	motor_set_vel(MOTOR3, 0, CLOSE_LOOP);
 }
 
-#define para_speed 1
+//parallel speed
+#define para_speed 30
 void chase(){
 	int16_t curr_x = get_X();
 	int16_t curr_y = get_Y();																																																																																																			
 	int16_t curr_angle = get_angle();
 	uint32_t curr_ticks = get_full_ticks();
-	int16_t pp_x, pp_y;
-
-	get_ppcor(curr_x, curr_y, &pp_x, &pp_y);
 	
-	int16_t pp_dis = distance(curr_x, curr_y, pp_x, pp_y);
-	if (curr_x>pp_x){
+	int16_t pp_dis = get_ppdis(curr_x, curr_y);
+	if (curr_y>((track_m*curr_x + track_c)/100)){
+		//if on the right side of the target line, switch direction
 		pp_dis = -pp_dis;
 	}
 
@@ -110,17 +115,17 @@ void chase(){
 	tft_prints(0, y_index++, "CX:%d CY:%d", curr_x, curr_y);
 	tft_prints(0, y_index++, "TX:%d TY:%d", target_x, target_y);
 	tft_prints(0, y_index++, "CBearing: %d", curr_angle);
-	tft_prints(0, y_index++, "PX:%d PY:%d", pp_x, pp_y);
-	tft_prints(0, y_index++, "PDis: %d", pp_dis);
+	tft_prints(0, y_index++, "PPdis: %d", pp_dis);
 
 	uint16_t target_dis = distance(target_x, target_y, curr_x, curr_y);
 	tft_prints(0, y_index++, "Distance: %d", target_dis);
 
+	//perpendicular speed
 	int16_t pp_v = pid(pp_dis);
-	//pp_v = 1;
 	
 	tft_prints(0, y_index++, "PV: %d", pp_v);
 	
+	//Vector addition
 	int16_t corr_bearing = int_arc_tan2(pp_v, para_speed) * 10;
 	
 	int16_t final_bearing = para_bearing + corr_bearing - curr_angle;
