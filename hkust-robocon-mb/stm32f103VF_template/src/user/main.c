b984bdf78fa21ef11310a4e1b6b76c9223befaeb
pkcwong <christopherpk1996@gmail.com>
2016-01-10 23:20:55 +0800

    update

--- hkust-robocon-mb/stm32f103VF_template/src/user/main.c
+++ hkust-robocon-mb/stm32f103VF_template/src/user/main.c
@@ -16,11 +16,15 @@ int W;
 int M;
 
 void _updateScreen();
-void _path(int x, int y, int angle);
+void _updateStatus();
+void _checkpoint(int x, int y, int angle);
+void _path();
 void _move(float magnitude, float bearing, float W);
 void _setTarget(int x, int y, int angle);
 int _getX();
 int _getY();
+void _delay();
+int _angleDiff(int o, int t);
 
 int main()
 {
@@ -30,10 +34,19 @@ int main()
 	can_init();
 	can_rx_init();
 	can_motor_init();
+	_delay();
+	//start
+	
+	_checkpoint(0, 1000, 90);
+	//_checkpoint(-1000, 1000, 180);
+	//_checkpoint(-1000, 0, 270);
+	//_checkpoint(0, 0, 0);
+	
 	while (1)
 	{
-		_path(250, 250, 0);
+		_updateStatus();
 		_updateScreen();
+		_path();
 	}
 }
 
@@ -45,43 +58,46 @@ void _updateScreen()
 	tft_prints(0,2,"Angle: %d", get_angle());
 	tft_prints(0, 3, "_move(%d, %d, %d)", M, target_direction, W);
 	tft_prints(0, 4, "MOTOR: %d %d %d", M1, M2, M3);
-	tft_prints(0, 5, "Target: %d %d", target_x, target_y);
+	tft_prints(0, 5, "Target: %d %d %d", target_x, target_y, target_angle);
 	tft_prints(0, 6, "t_dir: %d", target_direction);
 	tft_prints(0, 7, "dis: %d", dist);
 	tft_prints(0, 9, "timer: %d", get_ticks());
 	tft_update();
 }
 
-void _path(int x, int y, int angle)
+void _updateStatus()
+{
+	dist=Sqrt(Sqr(target_y-_getY())+Sqr(target_x-_getX()));
+	target_direction = ((90-(int_arc_tan2(target_y - _getY(), target_x - _getX()))) - get_angle()/10) %360;
+	target_direction = target_direction < 0 ? target_direction + 360 : target_direction;
+}
+
+void _checkpoint(int x, int y, int angle)
 {
 	_setTarget(x, y, angle);
-	while (dist>100 || Abs(target_angle-get_angle())>15)
+	_updateStatus();
+	_updateScreen();
+	_path();
+}
+
+void _path()
+{
+	while (dist>15 || Abs(target_angle-get_angle()/10)>5)
 	{
-		target_direction = (int_arc_tan2(target_y - _getY(), target_x - _getX()) - get_angle()/10) %360;
-		target_direction = target_direction < 0 ? target_direction + 360 : target_direction;
-		dist=Sqrt(Sqr(target_y-_getY())+Sqr(target_x-_getX()));
-		if (target_angle-angle>Abs(target_angle-angle))
-		{
-			W=360-(target_angle-angle);
-		}
-		else
-		{
-			W=target_angle-angle;
-		}
-		if (dist/10+10>100)
+		_updateStatus();
+		W=_angleDiff(get_angle()/10, target_angle)*140/180;
+		if (dist/10>100)
 		{
 			M=100;
 		}
 		else
 		{
-			M=dist/10+10;
+			M=dist/10;
 		}
-		_move(M, target_direction, W);
 		_updateScreen();
+		_move(M, target_direction*10, W);
 	}
-	motor_lock(MOTOR1);
-	motor_lock(MOTOR2);
-	motor_lock(MOTOR3);
+	_move(0, 0, 0);
 }
 
 void _move(float magnitude, float bearing, float W)
@@ -123,4 +139,26 @@ int _getX()
 int _getY()
 {
 	return get_pos()->x;
+}
+
+void _delay()
+{
+	int start=get_seconds();
+	while (get_seconds()-start!=5)
+	{
+		_updateScreen();
+		motor_lock(MOTOR1);
+		motor_lock(MOTOR2);
+		motor_lock(MOTOR3);
+	}
+}
+
+int _angleDiff(int o, int t)
+{
+	int a=t-o;
+	if (0>a && Abs(a)>180)
+	{
+		a=a+360;
+	}
+	return a;
 }
