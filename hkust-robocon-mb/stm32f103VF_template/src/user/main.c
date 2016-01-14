8cea233290a5a1acc1182677990e842a5eb20e31
Hong Wing Pang <panghw130@gmail.com>
2016-01-15 01:27:23 +0800

    Isolated parallel/perpendicular vectors

--- hkust-robocon-mb/stm32f103VF_template/src/user/main.c
+++ hkust-robocon-mb/stm32f103VF_template/src/user/main.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 
 #define THRESHOLD 50
-#define KP 0.6
 #define CONST_VEL 50
 
+/*
 struct target {
 	int x;
 	int y;
@@ -14,13 +14,18 @@ struct target {
 
 struct target tar_queue[50];
 int tar_head, tar_end;
+*/
 
-int tar_x, tar_y, tar_deg, cur_x, cur_y, cur_deg;
+int tar_x, tar_y, tar_deg, tar_dir;
+int cur_x, cur_y, cur_deg;
 int cor_x, cor_y;
-int v1, v2, v3;
-int degree, degree_diff, dist, dist_tar, speed;
+int ori_x, ori_y;
+int vel[3];
+int degree, degree_diff, dist, speed;
 int start, passed;
 
+
+/*
 void tar_enqueue(int x, int y, int deg) {
 	tar_queue[tar_head].x = x;
 	tar_queue[tar_head].y = y;
@@ -39,102 +44,113 @@ void tar_dequeue() {
 int tar_queue_length() {
 	return tar_head - tar_end;
 }
+*/
 
-void can_motor_set_angle(int angle, int rotate, int maxvel, double vel_coeff)
+void can_track_path(int angle, int rotate, int maxvel)
 {
-	angle *= 10;
-	v1 = int_sin(angle)*maxvel/(-10000);
-	v2 = int_sin(angle+1200)*maxvel/(-10000);
-	v3 = int_sin(angle+2400)*maxvel/(-10000);
+	int p, q;
+	int err;
+	double dotcheck;
 	
-	v1 -= rotate;
-	v2 -= rotate;
-	v3 -= rotate;	
-	motor_set_vel(MOTOR1, v1*vel_coeff, CLOSE_LOOP);
-	motor_set_vel(MOTOR2, v2*vel_coeff, CLOSE_LOOP);
-	motor_set_vel(MOTOR3, v3*vel_coeff, CLOSE_LOOP);
+	//determine velocity coefficient
+	double acc = passed / 2000.0;
+	if (acc > 1.0)
+		acc = 1.0;
+	double dec = dist / 600.0;
+	double vel_coeff = acc < dec ? acc : dec;
+	if (vel_coeff > 1.0)
+		vel_coeff = 1.0;
+	
+	//find perpendicular dist
+	p = tar_x - ori_x;
+	q = tar_y - ori_y;
+	err = p*(tar_y - cur_y) - q*(tar_x - cur_x);
+	err /= Sqrt(Sqr(p)+Sqr(q));
+	
+	//determine if overflow
+	dotcheck = (cur_x-tar_x)*(ori_x-tar_x) + (cur_y-tar_y)*(ori_y-tar_y);
+	dotcheck /= Sqrt(Sqr(ori_x-tar_x)+Sqr(ori_y-tar_y));
+	dotcheck /= Sqrt(Sqr(cur_x-tar_x)+Sqr(cur_y-tar_y));
+	if (dotcheck <= 0.0)
+		angle = 90 - int_arc_tan2(tar_y - cur_y, tar_x - cur_x) - (int)(cur_deg/10);
+	
+	//perpendicular KP
+	err *= 0.2;
+	
+	//rotational KP
+	rotate *= 0.5;
+	
+	angle *= 10;
+	for (int i = 0; i < 3; i++) {
+		vel[i] = int_sin(angle+i*1200)*maxvel/(-10000);	//calculate parallel distance
+		if (dotcheck > 0.0)
+			vel[i] -= int_sin(angle+i*1200+900)*err/(-10000);	//subtract perpendicular negative feedback
+		vel[i] *= vel_coeff;
+		vel[i] -= rotate*acc;	//subtract rotational negative feedback
+	}
+	
+	motor_set_vel(MOTOR1, vel[0], CLOSE_LOOP);
+	motor_set_vel(MOTOR2, vel[1], CLOSE_LOOP);
+	motor_set_vel(MOTOR3, vel[2], CLOSE_LOOP);
 }
 
 void can_motor_stop(){
-	v1 = 0;
-	v2 = 0;
-	v3 = 0;
-	motor_set_vel(MOTOR1, v1, CLOSE_LOOP);
-	motor_set_vel(MOTOR2, v2, CLOSE_LOOP);
-	motor_set_vel(MOTOR3, v3, CLOSE_LOOP);
+	vel[0] = 0;
+	vel[1] = 0;
+	vel[2] = 0;
+	motor_set_vel(MOTOR1, vel[0], CLOSE_LOOP);
+	motor_set_vel(MOTOR2, vel[1], CLOSE_LOOP);
+	motor_set_vel(MOTOR3, vel[2], CLOSE_LOOP);
 	motor_lock(MOTOR1);
 	motor_lock(MOTOR2);
 	motor_lock(MOTOR3);
 }
 
 void can_motor_update(){
-	passed = get_ticks() - start;
-	
-	cur_deg = get_angle();
-	cor_x = 0;
-	cor_y = 375;
-	xy_rotate(&cor_x, &cor_y, (-1)*cur_deg);
-	cur_x = get_X() + cor_x;
-	cur_y = get_Y() + cor_y - 375;
-	
-	degree = 90 - int_arc_tan2(tar_y - cur_y, tar_x - cur_x) - (int)(cur_deg/10);
-	degree = (degree+1080)%360;
-	degree_diff = tar_deg - (int)(cur_deg/10);
-	degree_diff = (degree_diff+1080)%360;
-	if (degree_diff > 180)
-		degree_diff -= 360;
-	
-	dist = Sqrt(Sqr(tar_x - cur_x) + Sqr(tar_y - cur_y));
-	dist_tar = Sqrt(Sqr(tar_queue[tar_head-1].x - cur_x) + Sqr(tar_queue[tar_head-1].y - cur_y));
-	
-	/*
-	//Self-rotation to measure correction vector
-	if (cur_deg <= 1800) {
-		motor_set_vel(MOTOR1, -20, CLOSE_LOOP);
-		motor_set_vel(MOTOR2, -20, CLOSE_LOOP);
-		motor_set_vel(MOTOR3, -20, CLOSE_LOOP);
-	} else {
+	if ((dist < THRESHOLD) && (Abs(degree_diff) < 5)) {
 		can_motor_stop();
-	}
-	*/
-	
-	//determine velocity coefficient
-	double acc = passed / 2000.0;
-	double dec = dist_tar / 600.0;
-	double vel = acc < dec ? acc : dec;
-	if (vel > 1.0)
-		vel = 1.0;
-	
-	//determine action taken
-	if (dist < 100) {
-		if (tar_queue_length()) {
-			tar_dequeue();
-		} else {
-			if ((dist < THRESHOLD) && (Abs(degree_diff) < 5))
-			can_motor_stop();
-		}
 	} else {
 		//if (dist_tar < 600) {
 		//	speed = (int)(dist_tar/12);
 		//} else {
-		can_motor_set_angle(degree, degree_diff, CONST_VEL, vel);
+		can_track_path(degree, degree_diff, CONST_VEL);
 	}
 	
 	//print debug info
 	tft_clear();
 	tft_prints(0,0,"X:%5d Y:%5d",cur_x,cur_y);
 	tft_prints(0,1,"ANGLE %d",cur_deg);
-	tft_prints(0,2,"TEST %d",speed);
-	tft_prints(0,3,"X:%5d Y:%5d",tar_x,tar_y);
-	tft_prints(0,4,"%5dmm  %3dd",dist,degree);
-	tft_prints(0,5,"ROTATE %3dd",degree_diff);
-	tft_prints(0,6,"QUEUE %2d-%2d",tar_end,tar_head);
-	tft_prints(0,7,"VEL %3d %3d %3d",v1,v2,v3);
-	tft_prints(0,8,"MAX %3d",speed);
+	//tft_prints(0,3,"X:%5d Y:%5d",tar_x,tar_y);
+	//tft_prints(0,4,"%5dmm  %3dd",dist,degree);
+	//tft_prints(0,5,"ROTATE %3dd",degree_diff);
+	tft_prints(0,7,"VEL %3d %3d %3d",vel[0],vel[1],vel[2]);
+	//tft_prints(0,8,"MAX %3d",speed);
 	tft_prints(0,9,"TIM %3d",get_seconds());
 	tft_update();
 }
 
+void can_calibrate(){
+	int cal_speed = -20;
+	if (cur_deg <= 150)
+		cal_speed = cur_deg/(-10)-5;
+	if (cur_deg >= 1600)
+		cal_speed = (1800-cur_deg)/(-10);
+	
+	if (cur_deg <= 1800) {
+		motor_set_vel(MOTOR1, cal_speed, CLOSE_LOOP);
+		motor_set_vel(MOTOR2, cal_speed, CLOSE_LOOP);
+		motor_set_vel(MOTOR3, cal_speed, CLOSE_LOOP);
+	} else {
+		can_motor_stop();
+	}
+	
+	tft_clear();
+	tft_prints(0,0,"X:%5d Y:%5d",cur_x,cur_y);
+	tft_prints(0,1,"ANGLE %d",cur_deg);
+	tft_update();
+}
+	
+/*
 void move_line(int x, int y, int deg, int segment) {
 	double ratio;
 	for (int i = 0; i < segment; i++) {
@@ -166,6 +182,7 @@ void move_bezier(int ax, int ay, int bx, int by, int segment) {
 		tar_enqueue(((qx - px)*ratio + px), ((qy - py)*ratio + py), 0);
 	}
 }
+*/
 
 int main(void)
 {
@@ -183,22 +200,46 @@ int main(void)
 	can_rx_init();
 	can_motor_init();
 	
-	tar_x = 0;
-	tar_y = 0;
-	tar_deg = 0;
 	_delay_ms(4000);
 	
-	tar_head = 0;
-	tar_end = 0;
+	//tar_head = 0;
+	//tar_end = 0;
 	
 	//set initial target pos	
 	ticks_init();
 	start = 0;
-	move_line(0, 2000, 0, 1);
-		
+	
+	ori_x = 0;
+	ori_y = 0;
+	tar_x = -1500;
+	tar_y = 1500;
+	tar_deg = -90;
+	tar_dir = 90 - int_arc_tan2(tar_y - cur_y, tar_x - cur_x);
+	
+	
 	while (1) {
 		if (get_ticks() % 50 == 0) {
+			
+			//update variables
+			passed = get_ticks() - start;
+			cur_deg = get_angle();
+			cor_x = 52;
+			cor_y = 380;
+			xy_rotate(&cor_x, &cor_y, (-1)*cur_deg);
+			cur_x = get_X() + cor_x - 52;
+			cur_y = get_Y() + cor_y - 380;
+			
+			degree = tar_dir - (cur_deg/10);
+			degree_diff = tar_deg - (int)(cur_deg/10);
+			degree_diff = (degree_diff+1080)%360;
+			if (degree_diff > 180)
+				degree_diff -= 360;
+			
+			dist = Sqrt(Sqr(tar_x - cur_x) + Sqr(tar_y - cur_y));
+			//dist_tar = Sqrt(Sqr(tar_queue[tar_head-1].x - cur_x) + Sqr(tar_queue[tar_head-1].y - cur_y));
+			
 			can_motor_update();
+			//can_calibrate();
 		}
 	}
 }
