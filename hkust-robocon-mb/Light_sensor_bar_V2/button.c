d51aae3a11472c3e044aba06c26e354b878743d7
Mokting <mt070709@hotmail.com>
2016-01-21 17:39:11 +0800

    Added Lightsensor programs from Robocon 2014

--- hkust-robocon-mb/Light_sensor_bar_V2/button.c
+++ hkust-robocon-mb/Light_sensor_bar_V2/button.c
@@ -2,24 +2,20 @@
 
 void button_init()
 {
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);	
 	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14; //button_2
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15; //button_1
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15; //button_2
 	GPIO_Init(GPIOC, &GPIO_InitStructure);	
 }
 
 u8 button_1()
 {
-	return(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_15));
+	return GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_15);
 }
 
 u8 button_2()
 {
-	return(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_14));
+	return GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_14);
 }
