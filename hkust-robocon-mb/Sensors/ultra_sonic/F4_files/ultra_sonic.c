4b6e3913778bc245060a339f0d63e2ac1cf28f77
Mokting <mt070709@hotmail.com>
2016-03-23 20:50:53 +0800

    Added F4_ultrasonic sensor library

--- hkust-robocon-mb/Sensors/ultra_sonic/F4_files/ultra_sonic.c
+++ hkust-robocon-mb/Sensors/ultra_sonic/F4_files/ultra_sonic.c
@@ -1,292 +1,203 @@
+/** Ultrasonic library
+*	@Author James Mok
+*	
+**/
+
 #include "ultra_sonic.h"
 
-static u16 last_sample_second = (u16) -1;
-static u8 last_sample_count = 0;
-static u8 sample_count = 0;
-
-static US_TypeDef us_devices[US_COUNT];
-static US_PROC_TypeDef us_proc[US_COUNT];
-
-
-static US_MODE us_mode = US_INDEPENDENT;
-
-static u8 current_us = 0;		// For US_TAKE_TURN
-//static u16 us_take_turn_break = 0;
+static u32 rising_ticks1 = 0;
+static u32 falling_ticks1 = 1;
+static u32 result1 = 0;
+static u16 result2 = 0;
+struct US_TypeDef US_struct[US_COUNT] = { {TRIG_PIN_1, ECHO_PIN_1}, {TRIG_PIN_2, ECHO_PIN_2} };
 
 void us_init()
 {
-	TM_GPIO_Init(US_PORT, TRIG_PIN_1, TM_GPIO_Mode_OUT, TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL, TM_GPIO_Speed_Fast);
-	TM_GPIO_Init(US_PORT, ECHO_PIN_1, TM_GPIO_Mode_IN, TM_GPIO_OType_PP, TM_GPIO_PuPd_DOWN, TM_GPIO_Speed_Fast);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_APB1PeriphClockCmd(US_RCC , ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	EXTI_InitTypeDef   EXTI_InitStructure;
 	
-	for(u8 i=0;i<US_COUNT;i++)
+	GPIO_InitTypeDef GPIO_InitStructure;
+	for(u8 i=0; i<US_COUNT; i++)
 	{
-		us_devices[i].trigger_time_us = 0;
-		us_devices[i].falling_time_us = 0;
-		us_devices[i].pulse_width_tmp = 0;
-		us_devices[i].pulse_width = 0;
-		us_devices[i].state = US_NULL;
+		GPIO_InitStructure.GPIO_Pin = US_struct[i].TRIG_Pin;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Fast_Speed;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+		GPIO_Init(US_PORT, &GPIO_InitStructure);
+		
+	
+		GPIO_InitStructure.GPIO_Pin = US_struct[i].ECHO_Pin;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Fast_Speed;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
+		GPIO_Init(US_PORT, &GPIO_InitStructure);
 	}
 	
-	GPIO_WriteBit(US_PORT, TRIG_PIN_1, 0);
-	
-	last_sample_count = 0;
-	last_sample_second = (u16) -1; 
-	sample_count = 0;
-	
-	NVIC_InitTypeDef NVIC_InitStructure;
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;      									// TimeBase is for timer setting   > refer to P. 344 of library
-  TIM_OCInitTypeDef  TIM_OCInitStructure;
-  EXTI_InitTypeDef   EXTI_InitStructure;
-	
-	RCC_APB2PeriphClockCmd(US_RCC , ENABLE);
-	GPIO_PinAFConfig(US_PORT, ECHO_PIN_1, GPIO_AF_TIM10);
-	
-	TIM_TimeBaseStructure.TIM_Prescaler = CLK / 1000000 - 1;     // 72M/1M - 1 = 71
-	TIM_TimeBaseStructure.TIM_Period = US_RESET_TIME;	
+												// TimeBase is for timer setting   > refer to P. 344 of library
+	TIM_TimeBaseStructure.TIM_Prescaler = US_TIM_CLK / 1000000 - 1;     // 84M/1M - 1 = 83
+	TIM_TimeBaseStructure.TIM_Period = US_RESET_TIME - 1;	
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(US_TIM, &TIM_TimeBaseStructure);      							 // this part feeds the parameter we set above
 	
-	TIM_ClearITPendingBit(US_TIM, TIM_IT_Update);												 // Clear Interrupt bits
-	TIM_ITConfig(US_TIM, TIM_IT_Update, ENABLE);													 // Enable TIM Interrupt
-  TIM_ITConfig(US_TIM, TIM_IT_CC1, ENABLE);													 // Enable TIM Interrupt
-	TIM_Cmd(US_TIM, ENABLE);																							 // Counter Enable
-  TIM_SetCounter(US_TIM, 0);
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;   		// set "high" to be effective output
+	TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCPolarity_High;   		// set "high" to be effective output
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;	           		// produce output when counter < CCR
+	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;		// Reset OC Idle state
+	TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Reset;	// Reset OC NIdle state
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;  	// this part enable the output
+	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputState_Disable; // this part disable the Nstate
+	TIM_OCInitStructure.TIM_Pulse = US_TRIG_PULSE;									// this part sets the initial CCR value
 	
-	
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;       //set "high" to be effective output
-  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;	              //produce output when counter < CCR
-  TIM_OCInitStructure.TIM_Pulse = US_TRIG_PULSE;                                     // 10us
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; 
-  TIM_OC1Init(US_TIM, &TIM_OCInitStructure);
-
-  TIM_ARRPreloadConfig(US_TIM, ENABLE);
-
-	
-	// EXIT & NVIC Init
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource6);
-  //GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource1);
-  EXTI_InitStructure.EXTI_Line = EXTI_Line1;
-  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
-  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-  EXTI_Init(&EXTI_InitStructure);
-	
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
-	NVIC_Init(&NVIC_InitStructure);
-//	
-//  GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource3);
-//  EXTI_InitStructure.EXTI_Line = EXTI_Line3;
-//  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-//  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
-//  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-//  EXTI_Init(&EXTI_InitStructure);
-//	
-//  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-//	NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;
-//	NVIC_Init(&NVIC_InitStructure);
-//	
-//	GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource5);
-//  EXTI_InitStructure.EXTI_Line = EXTI_Line5;
-//  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-//  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
-//  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-//  EXTI_Init(&EXTI_InitStructure);
-
-//	GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource7);
-//  EXTI_InitStructure.EXTI_Line = EXTI_Line7;
-//  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-//  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
-//  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-//  EXTI_Init(&EXTI_InitStructure);
-
-//	GPIO_EXTILineConfig(GPIO_PortSourceGPIOE, GPIO_PinSource9);
-//  EXTI_InitStructure.EXTI_Line = EXTI_Line9;
-//  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
-//  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
-//  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-//  EXTI_Init(&EXTI_InitStructure);
-	
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
-	NVIC_Init(&NVIC_InitStructure);
-	
-  
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStructure.NVIC_IRQChannel = US_IRQn;
 	NVIC_Init(&NVIC_InitStructure);
+	
+	if(US_COUNT>=1)
+	{
+		GPIO_PinAFConfig(US_PORT, GPIO_PinSource6, GPIO_AF_TIM3);			//GPIO AF Config for using PWM (do it for every GPIO AF!)
+		
+		TIM_OC1Init(US_TIM, &TIM_OCInitStructure);
+		TIM_OC1PreloadConfig(US_TIM, ENABLE);
+		
+		//EXTI setting
+		SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource9);
+		EXTI_InitStructure.EXTI_Line = EXTI_Line9;
+		EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+		EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+		EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+		EXTI_Init(&EXTI_InitStructure);
+		
+		//Interrupt seting
+	  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+		NVIC_InitStructure.NVIC_IRQChannel = EXTI9_5_IRQn;
+		NVIC_Init(&NVIC_InitStructure);
+		
+		EXTI_ClearITPendingBit(EXTI_Line9);													// Clear Interrupt bits
+	}
+	if(US_COUNT==2)
+	{
+		GPIO_PinAFConfig(US_PORT, GPIO_PinSource7, GPIO_AF_TIM3);
+		
+		TIM_OC2Init(US_TIM, &TIM_OCInitStructure);
+		TIM_OC2PreloadConfig(US_TIM, ENABLE);
+		
+		//EXTI setting
+		SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource10);
+		EXTI_InitStructure.EXTI_Line = EXTI_Line10;
+		EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+		EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+		EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+		EXTI_Init(&EXTI_InitStructure);
+		
+		//Interrupt seting
+		NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+		NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;
+		NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+		NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+		NVIC_Init(&NVIC_InitStructure);
+	
+		EXTI_ClearITPendingBit(EXTI_Line10);												// Clear Interrupt bits
+	}	
+	
+	TIM_ARRPreloadConfig(US_TIM, ENABLE);													
+	TIM_Cmd(US_TIM, ENABLE);	
+	TIM_CtrlPWMOutputs(US_TIM, ENABLE);
+	TIM_ITConfig(US_TIM, TIM_IT_Update, ENABLE);
+	TIM_ITConfig(US_TIM, TIM_IT_CC1, ENABLE);
+	
+	TIM_ClearITPendingBit(US_TIM, TIM_IT_Update);									// Clear Interrupt bits
+	TIM_ClearITPendingBit(US_TIM, TIM_IT_CC1);										// Clear Interrupt bits
 }
 
-static void us_trigger(u8 i)
+
+/**	@brief		Function for getting the raw data (pulse width in us)
+*		@param 		The us_device id(0,1)
+*		@retval		The u32 raw data  
+**/
+u16 get_result(u8 device)
 {
-	us_devices[i].trigger_time_us = 0;
-	us_devices[i].falling_time_us = 0;
-	GPIO_WriteBit(US_PORT, i, 1); // Trigger	
+	return US_struct[device].result;
 }
 
-
-US_MODE us_get_mode(void)
+/**	@brief		Function for getting the distance
+*		@param 		The us_device id(0,1)
+*		@retval		The u16 distance  
+**/
+u16 get_distance(u8 device)
 {
-	return us_mode;
-}
-
-US_STATE us_get_state(u8 i)
-{
-	if (i >= US_DEVICE_COUNT) {return US_READY;}
-	return us_devices[i].state;
-}
-
-u32 us_get_pulse_raw(u8 i)
-{
-	if (i >= US_DEVICE_COUNT) {return 0;}
-  return us_devices[i].pulse_width_tmp;
-}
-
-u32 us_get_pulse(u8 i)
-{
-	if (i >= US_DEVICE_COUNT) {return 0;}
-   return us_devices[i].pulse_width; 
-}
-
-u32 us_get_distance(u8 i)
-{
-	if (i >= US_DEVICE_COUNT) {return 0;}
-  return us_devices[i].pulse_width * 34 / 100 / 2;
-}
-
-u8 us_get_speed(void)
-{
-	if (last_sample_second != get_ticks()*1000) {
+	u16 temp;
+	temp = US_struct[device].result * 166/1000;
+	
+	if((temp < 30) || (temp > 2500))
 		return 0;
-	}
-	return last_sample_count; 
+	else
+		return temp;	
 }
 
-u8 us_get_current_us(void)
+void EXTI9_5_IRQHandler()
 {
-	return current_us;
-}
-
-void us_echo_interrupt(u8 i, u8 signal, u16 counter)
-{
-	if (signal) {
-		us_devices[i].trigger_time_us = counter;
-	} else {
-		us_devices[i].falling_time_us = counter;
-		if (us_devices[i].trigger_time_us == 0) {
-			us_devices[i].pulse_width = 0;
-		} else if (us_devices[i].trigger_time_us < us_devices[i].falling_time_us) {
-			us_devices[i].pulse_width = us_devices[i].falling_time_us - us_devices[i].trigger_time_us;
+	if(EXTI_GetITStatus(EXTI_Line9)!=RESET)
+	{
+		EXTI_ClearITPendingBit(EXTI_Line9);
+		if(GPIO_ReadInputDataBit(US_PORT, ECHO_PIN_1))
+		{
+			US_struct[0].rising_ticks = TIM_GetCounter(US_TIM);
 		}
-		//us_can_tx(i, us_get_distance(i));
-	}
-}
-
-u8 us_in_range(u8 i) 
-{
-	return us_get_distance(i) > 20 && us_get_distance(i) <= 1000;
-}
-
-void us_proc_init(void)
-{
-	for (u8 i = 0; i < US_DEVICE_COUNT; ++i) {
-		us_proc[i].in_range_full_ticks = 0;
-		us_proc[i].out_range_full_ticks = 0;
-		us_proc[i].in_distance = 0;
-		us_proc[i].in_range_time = 0;
-	}
-}
-
-void us_proc_update(void)
-{
-	for (u8 i = 0; i < US_DEVICE_COUNT; ++i) {
-		if (us_proc[i].in_range_full_ticks == 0 && us_in_range(i)) {
-			// Trigger
-			us_proc[i].in_range_full_ticks = get_ticks();
-			us_proc[i].in_distance = us_get_distance(i);
-			LED_ON(LED_2);
-		} else if (us_proc[i].in_range_full_ticks != 0 && !us_in_range(i)) {
-			// Falling
-			us_proc[i].out_range_full_ticks = get_ticks(); 
-			us_proc[i].in_range_time = us_proc[i].out_range_full_ticks - us_proc[i].in_range_full_ticks;
-			
-			us_proc[i].in_range_full_ticks = 0;
-			
-			//us_can_proc_tx(i, us_proc[i].in_range_time, us_proc[i].in_distance);
+		else
+		{
+			US_struct[0].falling_ticks = TIM_GetCounter(US_TIM);
+			if(US_struct[0].falling_ticks > US_struct[0].rising_ticks)
+				US_struct[0].result = US_struct[0].falling_ticks - US_struct[0].rising_ticks;
+			else
+				US_struct[0].result = 0;
 		}
 	}
 }
 
-void EXTI1_IRQHandler(void)
+void EXTI15_10_IRQHandler()
 {
-  if(EXTI_GetITStatus(EXTI_Line1) != RESET) {
-		EXTI_ClearITPendingBit(EXTI_Line1);
-		us_echo_interrupt(0, GPIO_ReadInputDataBit(US_PORT, ECHO_PIN_1), TIM_GetCounter(US_TIM));
-  }
+	if(EXTI_GetITStatus(EXTI_Line10)!=RESET)
+	{
+		EXTI_ClearITPendingBit(EXTI_Line10);
+		if(GPIO_ReadInputDataBit(US_PORT, ECHO_PIN_2))
+		{
+			US_struct[1].rising_ticks = TIM_GetCounter(US_TIM);
+		}
+		else
+		{
+			US_struct[1].falling_ticks = TIM_GetCounter(US_TIM);
+			if(US_struct[1].falling_ticks > US_struct[1].rising_ticks)
+				US_struct[1].result = US_struct[1].falling_ticks - US_struct[1].rising_ticks;
+			else
+				US_struct[1].result = 0;
+		}
+	}
 }
-
-//void EXTI3_IRQHandler(void)
-//{
-//  if(EXTI_GetITStatus(EXTI_Line3) != RESET) {
-//		EXTI_ClearITPendingBit(EXTI_Line3);
-//		us_echo_interrupt(1, gpio_read_input(us_devices[1].echo_gpio), TIM_GetCounter(US_TIM));
-//  }
-//}
-
-//void EXTI9_5_IRQHandler(void)
-//{
-//  if(EXTI_GetITStatus(EXTI_Line5) != RESET) {
-//		EXTI_ClearITPendingBit(EXTI_Line5);
-//		us_echo_interrupt(2, gpio_read_input(us_devices[2].echo_gpio), TIM_GetCounter(US_TIM));
-//  }
-//  if(EXTI_GetITStatus(EXTI_Line7) != RESET) {
-//		EXTI_ClearITPendingBit(EXTI_Line7);
-//		us_echo_interrupt(3, gpio_read_input(us_devices[3].echo_gpio), TIM_GetCounter(US_TIM));
-//  }
-//  if(EXTI_GetITStatus(EXTI_Line9) != RESET) {
-//		EXTI_ClearITPendingBit(EXTI_Line9);
-//		us_echo_interrupt(4, gpio_read_input(us_devices[4].echo_gpio), TIM_GetCounter(US_TIM));
-//  }
-//}
-
 
 US_IRQHandler
 {
-  if (TIM_GetITStatus(US_TIM, TIM_IT_Update) != RESET) {   
+  if (TIM_GetITStatus(US_TIM, TIM_IT_Update) != RESET) 
+	{   
 		// RESET
 		TIM_ClearITPendingBit(US_TIM, TIM_IT_Update);
-		for (u8 i = 0; i < US_COUNT; ++i) {
-			//us_can_tx(i, us_get_distance(i));
-			LED_ON(LED_3);
-			us_trigger(TRIG_PIN_1);
-		}
-    if (last_sample_second != get_ticks()/1000) {
-				last_sample_second = get_ticks()/1000; 
-				last_sample_count = sample_count; 
-				LED_ON(LED_4);
-				sample_count = 0;
-		}
-		++sample_count; 
-  }
-  
-  
-  if (TIM_GetITStatus(US_TIM, TIM_IT_CC1) != RESET) { 
+	}
+
+	if(TIM_GetITStatus(US_TIM, TIM_IT_CC1) != RESET)
+	{
 		TIM_ClearITPendingBit(US_TIM, TIM_IT_CC1);
-		for (u8 i = 0; i < US_COUNT; ++i) {
-			us_devices[i].trigger_time_us = 0;
-			us_devices[i].falling_time_us = 0;
-			GPIO_WriteBit(US_PORT, TRIG_PIN_1, 0); // Trigger
-		}
-    
-  } 
-}
+	}
+} 
+
+
--- hkust-robocon-mb/Sensors/ultra_sonic/F4_files/ultra_sonic.h
+++ hkust-robocon-mb/Sensors/ultra_sonic/F4_files/ultra_sonic.h
@@ -1,72 +1,39 @@
 #ifndef __ULTRA_SONIC_H
 #define __ULTRA_SONIC_H
 
-#include "tm_stm32f4_gpio.h"
-#include "tm_stm32f4_delay.h"
-#include "led.h"
+#include "stm32f4xx_gpio.h"
+#include "ticks.h"
 
-#define US_TIM 	TIM10
-#define US_RCC	RCC_APB2Periph_TIM10
-#define US_IRQn	TIM1_UP_TIM10_IRQn
-#define US_IRQHandler	void TIM1_UP_TIM10_IRQHandler()
+#define US_TIM 	TIM3
+#define US_RCC	RCC_APB1Periph_TIM3
+#define US_IRQn	TIM3_IRQn
+#define US_IRQHandler	void TIM3_IRQHandler()
 
-#define US_TRIG_PULSE         	6
-    // 10 us
-//#define US_ECHO_PULSE_COUNT   5
-//#define US_MAX_WIDTH					1000	// 3400mm
+#define US_TRIG_PULSE         	10
 
-//#define	US_IDLE_RESET_COUNT		60000		// 12 ms
-//#define	US_TAKE_TURN_BREAK		100
-
-#define CLK											72000000
-#define	US_RESET_TIME						30000	// 30ms
+#define US_TIM_CLK							84000000
+#define	US_RESET_TIME						30000 //update rate of sensor and the maximum detect range, update rate is inverse proprotion to update rate
 #define	US_DEVICE_COUNT					8
 
-#define US_PORT 	GPIOA
-#define TRIG_PIN_1 	7
-#define ECHO_PIN_1 	6
+#define US_PORT 					GPIOA
+#define TRIG_PIN_1 				GPIO_Pin_6
+#define ECHO_PIN_1 				GPIO_Pin_9
+#define TRIG_PIN_2 				GPIO_Pin_7
+#define ECHO_PIN_2 				GPIO_Pin_10
 
 #define US_COUNT		2
 
-typedef enum {
-	US_NULL								= 0,
-	US_READY							= 1,
-	US_TRIGGER 						= 2,
-	US_WAITING_FOR_ECHO		= 3,
-	US_ECHO_RAISED				= 4,
-	US_ECHO_FALLEN				= 5
-	
-} US_STATE;
-
-typedef struct {
-	//const GPIO* trig_gpio, *echo_gpio;
-	US_STATE state;
-	u32 pulse_width_tmp, pulse_width;
-	volatile u16 trigger_time_us, falling_time_us;
-} US_TypeDef;
-
-typedef enum {
-	US_INDEPENDENT,
-	US_SYNC,
-	US_TAKE_TURN
-} US_MODE;
-
-typedef struct {
-	u32 in_range_full_ticks, out_range_full_ticks;
-	u16 in_distance; 
-	u16 in_range_time;
-} US_PROC_TypeDef;
+struct US_TypeDef
+{
+	uint16_t TRIG_Pin;
+	uint16_t ECHO_Pin;	
+	u16 rising_ticks;
+	u16 falling_ticks;
+	u16 result;
+};
 
 void us_init();
-US_MODE us_get_mode(void);
-US_STATE us_get_state(u8 i);
-u32 us_get_pulse_raw(u8 i);
-u32 us_get_pulse(u8 i);
-u32 us_get_distance(u8 i);
-u8 us_get_speed(void);
-u8 us_get_current_us(void);
-void us_proc_init(void);
-void us_proc_update(void);
-u8 us_in_range(u8 i);
+u16 get_result(u8 device);
+u16 get_distance(u8 device);
 
 #endif
