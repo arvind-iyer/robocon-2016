a674757abfe467bacbb34d9bd0ec21797463c5c8
Rex <holyrexcheng@gmail.com>
2016-04-22 23:17:35 +0800

    added a lot of reference, working on DMP

--- Eco_base_1.0_board/STM32F405VG/Project/MDK-ARM/user/main.c
+++ Eco_base_1.0_board/STM32F405VG/Project/MDK-ARM/user/main.c
@@ -9,9 +9,9 @@ u32 any_loop_diff = 0;
 int main(void) {
 	SystemInit();
 	SystemCoreClockUpdate();
+	mpu_init();
 	ticks_init();
 	led_init();
-	ardu_adapter_init();
 	sensorbar_init();
 	servo_init();
 	si_init();
@@ -29,68 +29,71 @@ int main(void) {
 			
 			//Long loop action
 			any_loop_diff = this_loop_ticks - last_long_loop_ticks;
-			if (any_loop_diff > LONG_LOOP_TICKS){
-				led_blink(LED_D1);
-				tft_clear();
-				tft_println("[~BEETLE KING~]");
-				ardu_imu_update();
+			tft_clear();
+			tft_println("MPU6050 Testing");
+			tft_update();
+//			if (any_loop_diff > LONG_LOOP_TICKS){
+//				led_blink(LED_D1);
+//				tft_clear();
+//				tft_println("[~BEETLE KING~]");
+//				ardu_imu_update();
 
-				if (button_pressed(BUTTON_PIN)){
-					game_stage = IN_MENU;
-					menu_init();
-				}
-				
-				switch(game_stage){
-					case IN_MENU:
-						game_stage = menu_update();
-						break;
-					
-					case SYSTEM_WAITING:
-						if (ardu_all_ready()){
-							game_stage++;
-						}else{
-							tft_println("[SYSTEM WAITING]");
-						}
-						break;
-						
-					case CLIMBING_SLOPE:
-						tft_println("[CLIMBING]");
-						game_stage = path_up_update(); 
-						break;
-					
-					case CROSSING_RIVER:
-						tft_println("[RIVERING]");
-						game_stage = path_river_update();
-						break;
-					
-					case GOING_DOWN_HILL:
-						tft_println("[GOING DOWN]");
-						game_stage = path_down_update();
-						break;
-					
-					case WINNING_THE_GAME:
-						tft_println("[WIN OR FUCK UP]");
-						break;
-					
-					case PURE_SENSOR_BAR:
-						sensor_bar_track(PURE_SENSOR_BAR_POWER, PURE_SENSOR_BAR_Kp);
-						break;
-					
-					default:
-						tft_println("[WTF]");
-				}
-				
-				if (game_stage != IN_MENU){
-					tft_println("Loop: %d %d", this_loop_ticks, any_loop_diff);
-					tft_println("%d %d %d", ardu_int_ypr[0], ardu_int_ypr[1], ardu_int_ypr[2]);
-					
-					for (u8 i=0; i<16; i++){
-						tft_prints(i, 8, "%d", sensor_bar_value[i]);
-					}
-					tft_update();
-				}
-				last_long_loop_ticks = this_loop_ticks;
-			}
+//				if (button_pressed(BUTTON_PIN)){
+//					game_stage = IN_MENU;
+//					menu_init();
+//				}
+//				
+//				switch(game_stage){
+//					case IN_MENU:
+//						game_stage = menu_update();
+//						break;
+//					
+//					case SYSTEM_WAITING:
+//						if (ardu_all_ready()){
+//							game_stage++;
+//						}else{
+//							tft_println("[SYSTEM WAITING]");
+//						}
+//						break;
+//						
+//					case CLIMBING_SLOPE:
+//						tft_println("[CLIMBING]");
+//						game_stage = path_up_update(); 
+//						break;
+//					
+//					case CROSSING_RIVER:
+//						tft_println("[RIVERING]");
+//						game_stage = path_river_update();
+//						break;
+//					
+//					case GOING_DOWN_HILL:
+//						tft_println("[GOING DOWN]");
+//						game_stage = path_down_update();
+//						break;
+//					
+//					case WINNING_THE_GAME:
+//						tft_println("[WIN OR FUCK UP]");
+//						break;
+//					
+//					case PURE_SENSOR_BAR:
+//						sensor_bar_track(PURE_SENSOR_BAR_POWER, PURE_SENSOR_BAR_Kp);
+//						break;
+//					
+//					default:
+//						tft_println("[WTF]");
+//				}
+//				
+//				if (game_stage != IN_MENU){
+//					tft_println("Loop: %d %d", this_loop_ticks, any_loop_diff);
+//					tft_println("%d %d %d", ardu_int_ypr[0], ardu_int_ypr[1], ardu_int_ypr[2]);
+//					
+//					for (u8 i=0; i<16; i++){
+//						tft_prints(i, 8, "%d", sensor_bar_value[i]);
+//					}
+//					tft_update();
+//				}
+//				last_long_loop_ticks = this_loop_ticks;
+//			}
 			last_loop_ticks = this_loop_ticks;
 		}
 	}
