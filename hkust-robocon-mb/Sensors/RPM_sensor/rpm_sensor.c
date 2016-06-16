ac1cfb03cad002751aba8490fbd67b7a21fff917
shtamab <shtamab@connect.ust.hk>
2016-06-16 20:04:58 +0800

    Added TIM6 interrupt to auto update the rpm value, use RPMS_getVel() to get the velocity

--- hkust-robocon-mb/Sensors/RPM_sensor/rpm_sensor.c
+++ hkust-robocon-mb/Sensors/RPM_sensor/rpm_sensor.c
@@ -1,9 +1,6 @@
 #include "rpm_sensor.h"
 
-
 u32 RPMS_vel = 0;		//Counter value always positive
-
-
 
 /**
 	*	@brief	Initializes RPM sensor and count, hardware usage defined in "rpm_sensor.h"
@@ -13,6 +10,7 @@ u32 RPMS_vel = 0;		//Counter value always positive
 void RPMS_init(void)
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO|RCC_APB2Periph_GPIOE|RCC_APB2Periph_TIM1,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6 ,ENABLE);
 
 	RPMS_vel = 0;
 	
@@ -40,14 +38,42 @@ void RPMS_init(void)
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
   TIM_TimeBaseInit(RPMS_CNT_TIMx, &TIM_TimeBaseStructure);	
-
+	
 	// Sensor as the External Clock  Source
   TIM_TIxExternalClockConfig(RPMS_CNT_TIMx, TIM_TIxExternalCLK1Source_TI2, TIM_ICPolarity_Rising, 0x0);
 	//TIM_ETRClockMode1Config(RPMS_CNT_TIMx, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 0x00);
 	TIM_SelectSlaveMode(RPMS_CNT_TIMx, TIM_SlaveMode_External1);
 	
+	//Tim for calculating turning speed
+	TIM_TimeBaseStructure.TIM_Period = 50;	                 				       // update every 50ms
+	TIM_TimeBaseStructure.TIM_Prescaler = SystemCoreClock / 1000000 - 1;     // 72M/1M - 1 = 71
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(RPMS_UDT_TIMx, &TIM_TimeBaseStructure);
+	//Enable timer interrupt
+	TIM_ClearITPendingBit(RPMS_UDT_TIMx, TIM_IT_Update);												 // Clear Interrupt bits
+	TIM_ITConfig(RPMS_UDT_TIMx, TIM_IT_Update, ENABLE);													 // Enable TIM Interrupt
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannel = RPMS_UDT_IRQn;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	
 	TIM_CtrlPWMOutputs(RPMS_CNT_TIMx, ENABLE);
 	TIM_Cmd(RPMS_CNT_TIMx, ENABLE);
+	
+	TIM_ARRPreloadConfig(RPMS_UDT_TIMx, ENABLE);
+	TIM_Cmd(RPMS_UDT_TIMx, ENABLE);
+}
+
+void RPMS_UDT_Handler(void){
+	if (TIM_GetITStatus(RPMS_UDT_TIMx, TIM_IT_Update) != RESET) {
+    TIM_ClearFlag(RPMS_UDT_TIMx, TIM_FLAG_Update);
+		RPMS_vel = TIM_GetCounter(RPMS_CNT_TIMx);
+		TIM_SetCounter(RPMS_CNT_TIMx, 0);
+	}
 }
 
 
@@ -56,7 +82,7 @@ void RPMS_init(void)
 	* @param	
 	* @rtval	
 	**/
-void RPMS_update(void)
+u16 RPMS_update(void)
 {
 	RPMS_vel = TIM_GetCounter(RPMS_CNT_TIMx);
 	TIM_SetCounter(RPMS_CNT_TIMx, 0);
--- hkust-robocon-mb/Sensors/RPM_sensor/rpm_sensor.h
+++ hkust-robocon-mb/Sensors/RPM_sensor/rpm_sensor.h
@@ -5,11 +5,17 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_tim.h"
 
-#define RPMS_GPIO GPIOE
-#define RPMS_PIN GPIO_Pin_11
-#define RPMS_CNT_TIMx TIM1
+#define RPMS_GPIO 				GPIOE
+#define RPMS_PIN 					GPIO_Pin_11
+#define RPMS_CNT_TIMx 		TIM1
+
+#define RPMS_UDT_TIMx			TIM6
+#define RPMS_UDT_IRQn 		TIM6_IRQn
+#define RPMS_UDT_Handler	TIM6_IRQHandler
+
 
 void RPMS_init(void);
-u16 get_RPMS_cnt(void);
+u32 RPMS_getVel(void);
+u16 RPMS_update(void);
 
 #endif
