475a769282bd7eb3579cedcaf1a5ac83b524f77c
Mokting <mt070709@hotmail.com>
2016-06-18 20:19:12 +0800

    Updated method for better resolution in Smaller time scale, implementing PID and still need testing

--- hkust-robocon-mb/Sensors/RPM_sensor/RPM_sensor.c
+++ hkust-robocon-mb/Sensors/RPM_sensor/RPM_sensor.c
@@ -4,64 +4,123 @@
 
 #include "RPM_sensor.h"
 
-static volatile u32 past_val = 65535;
-static volatile u32 now_val = 0;
-static s32 diff = 0;
+static volatile u16 past_val = 0;
+static volatile u16 now_val = 0;
+static volatile u16 diff = 0;
+static u16 tar_val = 0;
+static u8 PID_FLAG = PID_ON;
+s32 PID;
 
-void RPMs_init(void){
-	encoder_init();
-	
+void RPMs_init(void){	
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
 	
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6 | RCC_APB1Periph_TIM5, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 	
+	GPIO_StructInit(&GPIO_InitStructure);																														// Set to default
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;						// Set Timer Pin 
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;																						// Set Pin mode to floating
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	//Init for RPMs_In_Tim
+	TIM_DeInit(RPMs_In_TIM);																													// clear
+	TIM_TimeBaseStructure.TIM_Prescaler = 0x00; 																										// No prescaling
+	TIM_TimeBaseStructure.TIM_Period = CountPerRo;																											// Max Count
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
+	TIM_TimeBaseInit(RPMs_In_TIM, &TIM_TimeBaseStructure);
+	TIM_EncoderInterfaceConfig(RPMs_In_TIM, TIM_EncoderMode_TI12,
+													 TIM_ICPolarity_Rising, TIM_ICPolarity_Rising);
+	
+	TIM_SetCounter(RPMs_In_TIM, 6);																													// Reset Count as 6
+	TIM_ClearITPendingBit(RPMs_In_TIM, TIM_IT_Update);
+	TIM_ITConfig(RPMs_In_TIM, TIM_IT_Update, ENABLE);
+	TIM_Cmd(RPMs_In_TIM, ENABLE);// Enable counter timer
+	
+	//Init for RPMs_Count_TIM
 	TIM_DeInit(RPMs_Count_TIM);
-	TIM_TimeBaseStructure.TIM_Period = 30000;	                 				       // Timer period, 30ms to trigger the event
-	TIM_TimeBaseStructure.TIM_Prescaler = SystemCoreClock / 1000000 - 1;     // 72M/1M - 1 = 71
+	TIM_TimeBaseStructure.TIM_Period = 450;	                 				       // Timer period, 450ms to trigger the event
+	TIM_TimeBaseStructure.TIM_Prescaler = RPMs_TIM_Period;     // 72M/1e5 - 1 = 710
 	TIM_TimeBaseInit(RPMs_Count_TIM, &TIM_TimeBaseStructure);      							 // this part feeds the parameter we set above
 	
 	TIM_ClearITPendingBit(RPMs_Count_TIM, TIM_IT_Update);												 // Clear Interrupt bits
 	TIM_ITConfig(RPMs_Count_TIM, TIM_IT_Update, ENABLE);													 // Enable TIM Interrupt
 	TIM_Cmd(RPMs_Count_TIM, ENABLE);																							 // Counter Enable
-
+	
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStructure.NVIC_IRQChannel = TIM6_IRQn;
-	NVIC_Init(&NVIC_InitStructure);	
-}
-
-u32 get_pulse(void){
-	return get_count(RPMs_encoder);
+	NVIC_Init(&NVIC_InitStructure);
 }
 
 /**
-``* @brief 	return the RPS. return -1 if in wrong direction
+``* @brief 	return the RPS
 ``*	@Param 	None
 	*	@retval RPS
 	*/
 u32 get_RPS(void){
-	s32 buffer = diff;
-	return buffer>0?buffer*1000 /(countPerRo * 30):buffer;
+	return 1 * diffScale / diff;
+}
+
+u32 get_diff(void){
+	return diff;
+}
+
+void set_tar_val(u16 tar){
+	tar_val = tar;
+}
+
+/**
+	* @breif 	PID for burshless, to be called in ticks handler
+	*	@Param 	P I D constant
+	*	@retval Nonw
+ **/
+void RPMs_update(u16 P, u16 I, u16 D){
+	static s16 now_error = 0;
+	static s16 past_error = 0;
+	static u16 now_pwm = 0;
+	
+	if(PID_FLAG == PID_OFF){
+		servo_control_all(450);
+	}
+	else{
+		now_error = tar_val - diff;
+	    
+		PID = P*now_error + I*(past_error + now_error) + D*(past_error + now_error)/2;
+	
+		now_pwm = 450 + PID;
+		past_error = now_error;
+	
+		now_pwm = now_pwm >= 900 ? 900 : now_pwm;
+		now_pwm = now_pwm <= 450 ? 450 : now_pwm;
+	
+		servo_control_all(now_pwm);
+	}
+}
+
+void TIM5_IRQHandler(void){
+	if (TIM_GetITStatus(TIM5, TIM_IT_Update) != RESET){
+    TIM_ClearFlag(TIM5, TIM_FLAG_Update);
+		diff = 1 * diffScale/TIM_GetCounter(RPMs_Count_TIM);
+		PID_FLAG = diff >= RPMs_UpLim ? PID_OFF : PID_ON;
+		
+		TIM_SetCounter(RPMs_Count_TIM, 0);
+	}
 }
 
 void TIM6_IRQHandler(void){
-	if (TIM_GetITStatus(RPMs_Count_TIM, TIM_IT_Update) != RESET) {
-    TIM_ClearFlag(RPMs_Count_TIM, TIM_FLAG_Update);
-		now_val = get_pulse();
-		if(now_val <= past_val)
-		{
-			diff = past_val - now_val;		//as the counter is down counting
-		}
-		else
-		{
-			diff = past_val + (65535 - now_val);
-		}
-		
-		if(diff > overFlow)
-			diff = -1;
-		
-		past_val = now_val;
+	if(TIM_GetITStatus(RPMs_Count_TIM, TIM_IT_Update) != RESET){
+		TIM_ClearFlag(RPMs_Count_TIM, TIM_FLAG_Update);
+		//PID_FLAG = PID_OFF;
+		diff = 0;
 	}
 }
--- hkust-robocon-mb/Sensors/RPM_sensor/RPM_sensor.h
+++ hkust-robocon-mb/Sensors/RPM_sensor/RPM_sensor.h
@@ -6,15 +6,27 @@
 #include "servo.h"
 #include "approx_math.h"
 
-#define RPMs_Count_TIM	TIM6
-#define RPMs_encoder		ENCODER2
 
-#define	countPerRo			6
-#define overFlow				60000
+#define RPMs_Count_TIM	TIM6
+#define RPMs_In_TIM			TIM5
+#define RPMs_PID_TIM		TIM9
+
+#define RPMs_UpLim			150
+#define	RPMs_DownLim		20
+
+#define RPMs_TIM_Period	SystemCoreClock / 100000 - 1
+#define	CountPerRo			6
+#define diffScale				10000
+
+enum PID_state{
+	PID_OFF = 0,
+	PID_ON};
 
 void RPMs_init(void);
 void RPMs_update(u16, u16, u16);
+void set_tar_val(u16);
 u32 get_pulse(void);
 u32 get_RPS(void);
+u32 get_diff(void);
 
 #endif
